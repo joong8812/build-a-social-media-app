13c4ace76883cb2b2bb39c762d4ce9dd8f07a751
joongmac <hotcoder@joongmacui-MacBook-Pro.local>
2020-03-31 16:51:55 -0400

    modified functions using Express (Get, Post)

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+    screams: [
+        {
+            userHandle: 'user',
+            body: 'this is the scream body',
+            createAt: '2020-03-31T20:30:02.573Z',
+            likecount: 5,
+            commentCount: 2
+        }
+    ]
+}
