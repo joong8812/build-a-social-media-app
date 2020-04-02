554c693e11249717e8bdb15c9e2d3b0e847249f5
joongmac <hotcoder@joongmacui-MacBook-Pro.local>
2020-04-01 23:22:44 -0400

    refactoring & organizing

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyCLZc497KBemtClIX6cNWd9qxGgn_scxWw",
+    authDomain: "socialape-ac3ba.firebaseapp.com",
+    databaseURL: "https://socialape-ac3ba.firebaseio.com",
+    projectId: "socialape-ac3ba",
+    storageBucket: "socialape-ac3ba.appspot.com",
+    messagingSenderId: "280500968286",
+    appId: "1:280500968286:web:7ee55c12620e7b7f9b8dcf",
+    measurementId: "G-QGTSZKXR33"
+  };
