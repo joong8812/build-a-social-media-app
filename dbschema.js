120bae4a27a6f1e04b182f48a3191c7659fd4e8c
joongmac <hotcoder@joongmacui-MacBook-Pro.local>
2020-04-03 16:32:11 -0400

    Add and Get User Profile Details

--- dbschema.js
+++ dbschema.js
@@ -1,4 +1,16 @@
 let db = {
+    users: [
+        {
+        userId: 'wekjrlj3owelv1',
+        email: 'user@gmail.com',
+        handle: 'user',
+        createAt: '2020-04-03T13:39:42.573Z',
+        imageUrl: 'image/asdkfjwqerqw/alsdjfoiq',
+        bio: 'Hello, my name is user, nice to meet you',
+        website: 'https://user.com',
+        location: 'Toronto, Canada'
+        }
+    ],
     screams: [
         {
             userHandle: 'user',
@@ -6,6 +18,30 @@ let db = {
             createAt: '2020-03-31T20:30:02.573Z',
             likecount: 5,
             commentCount: 2
+        }
+    ]
+}
+
+const userDetails = {
+    // Redux data
+    credentials: {
+        userId: 'QWELKRJ1234FJ321FJ',
+        email: 'user@gmail.com',
+        handle: 'user',
+        createAt: '2020-04-03T13:39:42.573Z',
+        imageUrl: 'image/asdkfjwqerqw/alsdjfoiq',
+        bio: 'Hello, my name is user, nice to meet you',
+        website: 'https://user.com',
+        location: 'Toronto, Canada'
+    },
+    likes: [
+        {
+            userHandle: 'user',
+            screamId: 'hh7O5WfWucVzGbHH2pa'
+        },
+        {
+            userHandle: 'user',
+            screamId: '3IOnFoQexRcofs5OhBXO'
         }
     ]
 }
